3c0fab8da8d687ed0686bd1dd52d9a6602b18bc8
KimTorLook <lokwahsmallho@gmail.com>
2023-05-18 16:12:30 +0800

    fea: 增加"加入遊戲"的"有第二個人加入"example的 pytest

--- backend/game/tests/model/test_game.py
+++ backend/game/tests/model/test_game.py
@@ -16,7 +16,7 @@ def test_createGame():
     assert game.state == GameState.waiting
 
 
-def test_joinGame():
+def test_頭一個玩家加入():
         
     # Arrange
     game_id = 1
@@ -30,3 +30,26 @@ def test_joinGame():
     assert len(game.players) == 1
     assert game.state == GameState.waiting
     assert game.host == player_id
+
+
+def test_有第二個人加入():
+
+    # Arrange > given
+    game_id = 2
+    game = Game.createGame(game_id)
+    player_id_A = 101
+    game.joinPlayer(player_id_A)
+    player_id_B = 102
+    
+    # Act > when
+    game.joinPlayer(player_id_B)
+
+    # Assert > then
+    assert len(game.players) == 2
+    assert game.state == GameState.waiting
+    assert game.host == player_id_A
+
+
+
+
+    
--- backend/game/uno/model/game.py
+++ backend/game/uno/model/game.py
@@ -11,9 +11,37 @@ class GameState(Enum):
 
 class Game:
 
-    id = 0
-    state = GameState.waiting
-    players: List[int] = []
+    # id = 0 # static variable
+    # state = GameState.waiting # static variable
+    # players: List[int] = [] # static variable
+
+    # attribte_a = 1234
+
+    def __init__(self, id: int, state: GameState = GameState.waiting, players: List[int] = []):
+        print("init")
+        self.id = id
+        self.state = state
+        self.players = players
+
+        # self.attribte_b = 5678
+        # self._attribte_c = 9999
+        # self._attribte_d = 9999
+        
+    # def get_attribte_c(self):
+    #     return self._attribte_c
+    # def set_attribte_c(self, value):
+    #     self._attribte_c = value
+
+    # Python setter 和 getter
+    # https://medium.com/bryanyang0528/python-setter-%E5%92%8C-getter-6c08a9d37d46
+
+    # @property # 裝飾器
+    # def attribte_d(self):
+    #     return self._attribte_d
+    
+    # @attribte_d.setter
+    # def set_attribte_d(self, value):
+    #     self._attribte_d = value
 
     @property
     def host(self) -> Optional[int]:
@@ -21,14 +49,28 @@ class Game:
             return None
         return self.players[0]
 
+    # @host.setter #不寫代表唯讀
+    # def set_host(self, value):
+    #     pass
+
     @staticmethod
     def createGame(id: int):
-        result = Game(id, GameState.waiting)
+        result = Game(id, GameState.waiting, [])
         return result
-
-    def __init__(self, id: int, state: GameState):
-        self.id = id
-        self.state = state
 
     def joinPlayer(self, player_id: int):
         self.players.append(player_id)
+
+# test = Game.createGame(1)
+
+# something = test.attribte_a #取值
+# test.attribte_a = 123 #賦值
+
+# something = test.attribte_b #取值
+# test.attribte_b = 123 #賦值
+
+# something = test.get_attribte_c() #取值
+# test.set_attribte_c(123) #賦值
+
+# something = test.attribte_d #取值
+# test.attribte_d = 123 #賦值
