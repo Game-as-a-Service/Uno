41b57910f30af3f1f8abb2fd42f63c955014642f
w91379137 <w913791379137@gmail.com>
2023-06-03 02:46:33 +0800

    [feat] 實作比大小

--- backend/game/uno/model/deck.py
+++ backend/game/uno/model/deck.py
@@ -1,8 +1,14 @@
 from typing import List
-from card import Card
+from .card import Card
 
 class Deck:
 
     def __init__(self, player: int, cardList: List[Card]):
         self.player: int = player
         self.cardList = cardList
+
+    def __gt__(self, other):
+        if isinstance(other, Deck):
+            return self.cardList[0].symbol > other.cardList[0].symbol
+        else:
+            raise Exception("cannot compare(>) Deck and {}".format(type(other)))
