2ce32898faf7aa303f76942820b525f312f0d545
w91379137 <w913791379137@gmail.com>
2023-06-18 13:58:31 +0800

    [feat] 新建需要畫面,進入第一頁

--- frontend/uno-web/src/app/app.module.ts
+++ frontend/uno-web/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartComponent } from './page/start/start.component';
+import { LobbyComponent } from './page/lobby/lobby.component';
+import { WaitComponent } from './page/wait/wait.component';
+import { GameComponent } from './page/game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    LobbyComponent,
+    WaitComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
