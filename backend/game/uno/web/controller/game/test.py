3edf66331fd6b244d299c916728b2c107898ce3b
w91379137 <w913791379137@gmail.com>
2023-07-02 16:25:43 +0800

    [fix] 修正沒有注入

--- backend/game/uno/web/controller/game/test.py
+++ backend/game/uno/web/controller/game/test.py
@@ -3,10 +3,10 @@ from uno.usecase.test_usecase import Testusecase
 from dependency_injector.wiring import inject, Provide
 from uno.web.containers import Container
 
-
+@inject
 def test2(usecase: Testusecase = Provide[Container.testUsecase]):
     print(type(usecase))
-    output = usecase.execute
+    output = usecase.execute()
     print(output)
-    return "hello"
+    return output
 
