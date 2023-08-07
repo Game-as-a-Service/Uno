798b2aba75c31fb11bf6497a5f6ed8b95a6eafab
w91379137 <w913791379137@gmail.com>
2023-08-08 05:37:27 +0800

    [fix] 修正 usecase 的 bdd 測試

--- backend/game/features/steps/steps.py
+++ backend/game/features/steps/steps.py
@@ -2,7 +2,7 @@
 # https://github.com/ictar/python-doc/blob/master/Testing/%E5%9C%A8Python%E4%B8%AD%E4%BD%BF%E7%94%A8Behave%E6%9D%A5%E5%BC%80%E5%A7%8B%E8%A1%8C%E4%B8%BA%E6%B5%8B%E8%AF%95.md
 
 from behave import *
-from uno.usecase.create_game_usecase import CreateGameUseCase
+from uno.usecase.create_game_usecase import CreateGameUseCase, CreateGameUseCaseInput, CreateGameUseCaseOutput
 from uno.repository.game_repository_in_memory_impl import GameRepositoryInMemoryImpl
 from uno.model.game import Game
 
@@ -14,7 +14,10 @@ def step_impl(context, id):
 def step_impl(context):
     usecase = CreateGameUseCase(GameRepositoryInMemoryImpl())
     context.usecase = usecase
-    usecase.execute(context.gameId)
+    input = CreateGameUseCaseInput()
+    output = CreateGameUseCaseOutput()
+    input.game_id = context.gameId
+    usecase.execute(input, output)
 
 @then(u'存在一個遊戲: {id:d}')
 def step_impl(context, id):
